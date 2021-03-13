009c102d06d56f6caee3fc89ddb570afa3ef68a4
Suraj Jindam <57936889+SurajJindam@users.noreply.github.com>
2021-03-13 19:11:52 +0530

    Update Solution.java

--- firstProblem/Solution.java
+++ firstProblem/Solution.java
@@ -1,1 +1,15 @@
-
+import java.util.*;
+class Solution{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n-1];
+		int sum = 0;
+		for(int i=0;i<n-1;i++){
+			arr[i] = sc.nextInt();
+			sum+=arr[i];
+		}
+		int act_sum = (int)((n*(n+1))/2);
+		System.out.println("Missing number is : "+(act_sum - sum));
+	}
+}
